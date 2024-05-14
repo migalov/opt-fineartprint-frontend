4c5b8fe9621671acec65eb02d8bf8468e5e88554
Michel Muniagiseny <mafri.michel@gmail.com>
2024-05-14 15:33:45 +0300

    Added API_BACKEND_DOMAIN to ItemMenu.vue

--- components/ItemMenu.vue
+++ components/ItemMenu.vue
@@ -1,11 +1,11 @@
 <template>
   <li>
     <NuxtLink v-if="url" :to="url" :class="`${size} flex items-center gap-2`">
-      <NuxtImg :width="12" v-if="icon_url" :src="`http://localhost:1337${icon_url}`" />
+      <NuxtImg :width="12" v-if="icon_url" :src="`${API_BACKEND_DOMAIN}${icon_url}`" />
       <span>{{ title }}</span>
     </NuxtLink>
     <div v-else class="flex items-center gap-2">
-      <NuxtImg v-if="icon_url" :width="12" :src="`http://localhost:1337${icon_url}`" />
+      <NuxtImg v-if="icon_url" :width="12" :src="`${API_BACKEND_DOMAIN}${icon_url}`" />
       <span :class="`${size}`">{{ title }}</span>
     </div>
     
@@ -28,6 +28,8 @@
       type: String
     }
   })
+  const config = useRuntimeConfig(),
+        API_BACKEND_DOMAIN = config.public.api_base;
 </script>
 
 <style scoped>
